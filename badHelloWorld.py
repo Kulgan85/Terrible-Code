7664785f684c77a38c1d88579d3b87b89547fbef
Kulgan85 <54630904+Kulgan85@users.noreply.github.com>
2021-05-12 15:37:02 +0930

    Add files via upload

---
+++ badHelloWorld.py
@@ -0,0 +1,23 @@
+theultimatevariablenamethatissupercoolanddetermineswhatthetruemeaningoftheuniverseis = True
+intentionallybadlistname1=["J","B","d","V","O","f","x","i","L","e","r","Z","k","S","h","W","m","c","t","a","u","q","n","y","G","p"]
+intentionallybadlistname2=[14,0,18,1,20,11,23,2,9,4,15,25,12,6,10,22,17,13,7,19,5,16,3,21,8,24]
+for i in intentionallybadlistname2:
+    intentionallybadlistname2.sort(reverse=True)
+for i in range(len(intentionallybadlistname1)):
+    intentionallybadlistname1[i]=intentionallybadlistname1[i].lower()
+intentionallybadlistname1.sort()
+intentionallybadvariableone=intentionallybadlistname2[18]
+intentionallybadvariabletwo=intentionallybadlistname2[21]
+intentionallybadvariablethree=intentionallybadlistname2[14]
+intentionallybadvariablefour=intentionallybadlistname2[14]
+intentionallybadvariablefive=intentionallybadlistname2[11]
+gapmoe=" "
+intentionallybadvariablesix=intentionallybadlistname2[3]
+intentionallybadvariableseven=intentionallybadlistname2[11]
+intentionallybadvariableeight=intentionallybadlistname2[8]
+intentionallybadvariablenine=intentionallybadlistname2[14]
+intentionallybadvariableten=intentionallybadlistname2[22]
+if theultimatevariablenamethatissupercoolanddetermineswhatthetruemeaningoftheuniverseis == True:
+    print(intentionallybadlistname1[intentionallybadvariableone].upper() + intentionallybadlistname1[intentionallybadvariabletwo] + intentionallybadlistname1[intentionallybadvariablethree] + intentionallybadlistname1[intentionallybadvariablefour] + intentionallybadlistname1[intentionallybadvariablefive] + gapmoe + intentionallybadlistname1[intentionallybadvariablesix].upper() + intentionallybadlistname1[intentionallybadvariableseven] + intentionallybadlistname1[intentionallybadvariableeight] + intentionallybadlistname1[intentionallybadvariablenine] + intentionallybadlistname1[intentionallybadvariableten])
+else:
+    print("Hello World")
